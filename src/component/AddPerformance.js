f4a1328159de02519d031a8237c2154a5fe91750
Muthiira <calebmuchiri48@gmail.com>
2022-10-05 22:58:28 +0300

    styling

--- src/component/AddPerformance.js
+++ src/component/AddPerformance.js
@@ -1,60 +1,75 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 import { post, patch } from "./crud";
+import TextField from '@mui/material/TextField';
 
-export default function AddPerformance ({onSaved, defaultData}){
 
-// deriving input values from state(making it controlled)
+export default function AddPerformance({ onSaved, defaultData }) {
+
+	// deriving input values from state(making it controlled)
 	const [formData, setFormData] = useState({
 		Student: defaultData ? defaultData.Student : "",
-		Performance: defaultData ?  defaultData.Performance : "",
-		Target: defaultData ?   defaultData.Target : ""
-});
+		Performance: defaultData ? defaultData.Performance : "",
+		Target: defaultData ? defaultData.Target : ""
+	});
 
-// including loader
-const [saving, setSaving] = useState(false)
+	// including loader
+	const [saving, setSaving] = useState(false)
 
-// function that handles change
-	function handleChange(event){
+	// function that handles change
+	function handleChange(event) {
 		setFormData({
 			...formData,
-			[event.target.name] :  event.target.value
+			[event.target.name]: event.target.value
 		})
 	}
 
-// update function on submission
-	function addResult(event){
+	// update function on submission
+	function addResult(event) {
 		event.preventDefault();
 		setSaving(true)
-// if defaultData is defined update,if not defined create
-		if(defaultData){
-			patch({...formData, id:defaultData.id}).then((updatedPerformance) => {
-			setSaving(false);
-			onSaved(updatedPerformance);
-			console.log(updatedPerformance);
+		// if defaultData is defined update,if not defined create
+		if (defaultData) {
+			patch({ ...formData, id: defaultData.id }).then((updatedPerformance) => {
+				setSaving(false);
+				onSaved(updatedPerformance);
+				console.log(updatedPerformance);
 			})
-		}else{
-		post(formData).then(newResult =>{
-			setSaving(false);
-			onSaved(newResult);
-			console.log(newResult);
-		});}
+		} else {
+			post(formData).then(newResult => {
+				setSaving(false);
+				onSaved(newResult);
+				console.log(newResult);
+			});
+		}
 	}
+	return (<form  onSubmit={addResult}>
+		<div>
+			<TextField id="filled-basic" label="Student" variant="filled" type="text" name="Student" value={formData.Student} onChange={handleChange} />
+			<TextField id="filled-basic" label="Performance" variant="filled" type="text" name="Performance" value={formData.Performance} onChange={handleChange} />
+			<TextField id="filled-basic" label="Target" variant="filled" type="text" name="Target" value={formData.Target}  onChange={handleChange}/>
 
-	return <form onSubmit={addResult}>
-		<div>
-			<label>Student</label>
-			<input type="text" name="Student" value={formData.Student}  onChange={handleChange}/>
 		</div>
-		<div>
-			<label>Perfomance</label>
-			<input type="text" name="Performance" value={formData.Performance}  onChange={handleChange}/>
-		</div>
-		<div>
-			<label>Target</label>
-			<input type="text" name="Target" value={formData.Target}  onChange={handleChange}/>
-		</div>
-		<div>
-			<button disabled={saving}>{saving ? 'saving...' : 'save result'}</button>
-		</div>
-	</form>
+		</form>
+	)
+
+
+
+	// return <form onSubmit={addResult}>
+	// 	<div>
+	// 		<label>Student</label>
+	// 		<input type="text" name="Student" value={formData.Student}  onChange={handleChange}/>
+
+	// 	</div>
+	// 	<div>
+	// 		<label>Perfomance</label>
+	// 		<input type="text" name="Performance" value={formData.Performance}  onChange={handleChange}/>
+	// 	</div>
+	// 	<div>
+	// 		<label>Target</label>
+	// 		<input type="text" name="Target" value={formData.Target}  onChange={handleChange}/>
+	// 	</div>
+	// 	<div>
+	// 		<button disabled={saving}>{saving ? 'saving...' : 'save result'}</button>
+	// 	</div>
+	// </form>
 }
--- src/component/App.js
+++ src/component/App.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import AddPerformance from "./AddPerformance";
 import StudentResult from "./StudentResult";
 import { collect, Delete } from "./crud";
+import Button from '@mui/material/Button';
+
 
 
 function App() {
@@ -9,35 +11,35 @@ function App() {
 	const [studentsResult, setStudentsResult] = useState([]);
 	const [newUpdate, setNewUpdate] = useState();
 
-// handling side effect
+	// handling side effect
 	useEffect(() => {
 		collect().then(results => {
 			setStudentsResult(results)
 		})
 	}, [])
-//  callback function to update results
+	//  callback function to update results
 	function updateResults(newResult) {
-// if newUpdate is defined update result if not defined add into the array
-		if(newUpdate){
+		// if newUpdate is defined update result if not defined add into the array
+		if (newUpdate) {
 			const index = studentsResult.findIndex(r => r.id == newResult.id);
 			console.log(index)
-// updating form using index
+			// updating form using index
 			let latestUpdate = [...studentsResult];
 			latestUpdate[index] = newResult;
 			setStudentsResult(latestUpdate);
 
-		}else{
+		} else {
 			setStudentsResult([...studentsResult, newResult]);
 		}
 		setAddingPerformance(false);
 	}
-// function that handles patch(edit)
+	// function that handles patch(edit)
 	function onUpdate(result) {
 		setNewUpdate(result)
 		setAddingPerformance(true)
 	}
-// function that handles delete
-	function onDelete(id){
+	// function that handles delete
+	function onDelete(id) {
 		Delete(id).then(resp => {
 			const index = studentsResult.findIndex(r => r.id == id);
 			console.log(index)
@@ -51,10 +53,10 @@ function App() {
 	return (
 
 		<main>
-			<button type="button" onClick={() => setAddingPerformance(!addingPerformance)} >{addingPerformance ? "cancel" : "addPerformance"}</button>
 			{addingPerformance ?
 				<AddPerformance onSaved={updateResults} defaultData={newUpdate} /> :
-				<StudentResult StudentResult={studentsResult} update={onUpdate}  Delete={onDelete}/>}
+				<StudentResult StudentResult={studentsResult} update={onUpdate} Delete={onDelete} />}
+			<Button  variant="outlined" type="button" onClick={() => setAddingPerformance(!addingPerformance)} >{addingPerformance ? "cancel" : "addPerformance"}</Button>
 
 		</main>
 	)
